cd2e4b5e9aedde76bc2c7e018ba9ac6e51552e29
rajila598 <130640563+rajila598@users.noreply.github.com>
2024-04-25 14:53:37 +0545

    database

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 const mongoose = require("mongoose");
 const url = (process.env.NODE_ENV == "production" ? process.env.MONGODB_URL_PROD : process.env.MONGODB_URL_DEV);
-mongoose.connect(url).then(() => console.log("Connected!"));
+mongoose.connect(url).then(() => console.log("Connected to",url));
